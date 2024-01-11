ffbafd374a11b512fae04d8df3d0defc248a6127
Kelly Xiang <154550100+339465916@users.noreply.github.com>
2024-01-11 13:37:54 -0500

    Add files via upload

---
+++ res/MainGameCode.java
@@ -0,0 +1,20 @@
+/*
+ * MainGameCode.java
+ * By Emma Lane-Smith, Julie Pham, and Kelly Xiang
+ * Jan 11, 2024
+ * Main code for Not a Forking Clue
+ */
+package isp;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+public class MainGameCode {
+
+	public static void main(String[] args) {
+		//
+
+	}
+
+}
