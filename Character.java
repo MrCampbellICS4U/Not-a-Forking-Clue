e48b71542703345996cff261355eb14ae2dc4960
juliepham07 <154549553+juliepham07@users.noreply.github.com>
2024-01-15 13:29:45 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,5 +1,10 @@
 package isp;
-
+/*
+ * Character.java
+ * By Emma Lane-Smith, Julie Pham, and Kelly Xiang
+ * Jan 11, 2024
+ * Character class for Not a Forking Clue
+ */
 public class Character {
 
 }
