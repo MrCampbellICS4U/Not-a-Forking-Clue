9723af9931b0347b9a1af328e326cf2d274bd743
Kelly Xiang <154550100+339465916@users.noreply.github.com>
2024-01-15 18:51:23 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,10 +1,78 @@
-package isp;
 /*
  * Character.java
  * By Emma Lane-Smith, Julie Pham, and Kelly Xiang
- * Jan 11, 2024
- * Character class for Not a Forking Clue
+ * Jan 12, 2023
+ * Abstract class for character profiles: includes things like name, age, job, etc.
  */
-public class Character {
+package grade12;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+public class Character {
+	String name, role;
+	int age;
+	
+	/**
+	 * class constructor
+	 * @param name	character's name
+	 * @param age	character's age
+	 * @param role	character's role/job
+	 */
+	public Character(String name, int age, String role) {
+		this.name = name;
+		this.age = age;
+		this.role = role;
+	}
+	
+	/**
+	 * getter method for character's name
+	 * @return 	character's name
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/**
+	 * setter method for character's name
+	 * @param name	character's name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * getter method for character's role
+	 * @return	character's role
+	 */
+	public String getRole() {
+		return this.role;
+	}
+	
+	/**
+	 * setter method for character's role
+	 * @param role	character's role
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	/**
+	 * getter method for character's age
+	 * @return	character's age
+	 */
+	public int getAge() {
+		return this.age;
+	}
+	
+	/**
+	 * setter method for character's age
+	 * @param age	character's age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
